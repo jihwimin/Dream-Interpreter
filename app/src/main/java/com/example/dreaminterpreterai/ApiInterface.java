88209db0b03d07b644841e7db779b6b9a75c3f8b
jihwimin <143287844+jihwimin@users.noreply.github.com>
2024-10-28 00:58:16 +0900

    API KEY

--- app/src/main/java/com/example/dreaminterpreterai/ApiInterface.java
+++ app/src/main/java/com/example/dreaminterpreterai/ApiInterface.java
@@ -9,7 +9,7 @@ import retrofit2.http.POST;
 public interface ApiInterface {
     @Headers({
             "Content-Type: application/json",
-            "Authorization: Bearer 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"
+            "Authorization: Bearer API KEY"
     })
     @POST("chat/completions")
     Call<ChatResponse> getDreamInterpretation(@Body ChatRequest request);
